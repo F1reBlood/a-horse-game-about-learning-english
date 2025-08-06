25906b5f39d2e2b02c255bb119ae1ea38184a213
F1reBlood <tristan.turki4@gmail.com>
2025-08-06 20:42:32 +0200

    Ajout des boutons play, character, settings, quit et de l'arrière plan + ajout du style sur les boutons

--- src/GUI/MainMenu.java
+++ src/GUI/MainMenu.java
@@ -1,14 +1,61 @@
 package GUI;
 
+import com.sun.jdi.event.MethodExitEvent;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class MainMenu {
+    private void addStyleToButton(JButton button){
+        button.setBackground(new Color(70,130,180));
+        button.setForeground(Color.white);
+        button.setFocusPainted(false);
+        button.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        button.setFont(new Font("Arial", Font.BOLD, 20));
+        button.setBorder(BorderFactory.createEmptyBorder(10, 20, 10, 20));
+
+        button.addMouseListener(new MouseAdapter() {
+            public void mouseEntered(MouseEvent e) {
+                button.setBackground(new Color(75,153,212));
+            }
+            public void mouseExited(MouseEvent e) {
+                button.setBackground(new Color(70,130,180));
+            }
+        });
+    }
+
     public MainMenu() {
         JFrame frame = new JFrame();
         JPanel panel = new JPanel();
 
-        panel.setLayout(new GridLayout(0, 1));
+        ImageIcon backgroundIcon = new ImageIcon(getClass().getResource("/Images/horse_bg.jpg"));
+        JLabel backgroundLabel = new JLabel(backgroundIcon);
+        backgroundLabel.setLayout(new GridBagLayout());
+        backgroundLabel.setSize(1280, 720);
+        frame.setContentPane(backgroundLabel);
+
+        backgroundLabel.add(panel);
+
+        panel.setOpaque(false);
+        panel.setLayout(new GridLayout(4,1, 0, 30));
+
+        JButton play = new JButton("Play");
+        JButton character = new JButton("Character");
+        JButton settings = new JButton("Settings");
+        JButton exit = new JButton("Exit");
+
+        addStyleToButton(play);
+        addStyleToButton(character);
+        addStyleToButton(settings);
+        addStyleToButton(exit);
+
+        panel.add(play);
+        panel.add(character);
+        panel.add(settings);
+        panel.add(exit);
+        exit.addActionListener(e -> System.exit(0));
 
         // Obtenir la taille de l'écran en tant que dimension Java
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -22,11 +69,11 @@ public class MainMenu {
 
         frame.setMinimumSize(new Dimension(1280,720));
         frame.setMaximumSize(new Dimension(1280,720));
-        frame.add(panel);
         frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
         frame.setTitle("A horse game about learning english");
         frame.pack();
         frame.setVisible(true);
+
     }
 
 }
