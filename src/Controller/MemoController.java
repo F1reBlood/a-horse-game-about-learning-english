c94eef1fa5ebd92bbf431e6c10908e00369272d8
FireBlood <tristan.turki4@gmail.com>
2025-08-11 10:53:51 +0200

    Ajout de commentaires dans MemoController

--- src/Controller/MemoController.java
+++ src/Controller/MemoController.java
@@ -21,10 +21,13 @@ public class MemoController {
         this.model = model;
         this.pairList = pairList;
 
+        // Sépare les mots des paires afin de faire une liste mélangée de mots
         List<String> mots = MemoLogic.listMots(pairList);
 
+        // Crée un dictionnaire qui relie un mot au bouton portant ce mot
         Dictionary<String, JButton> buttons = new Hashtable<>();
 
+        // Crée les boutons et les associe chacun à un mot différent, leur ajoute un style, les ajoute au dictionnaire et les affiche
         for (int i = 0; i < mots.size(); i++) {
             JButton button = new JButton(mots.get(i));
             MainMenu.addStyleToButton(button);
@@ -32,14 +35,17 @@ public class MemoController {
             this.view.getPanel().add(button);
         }
 
+        // Crée un timer de 10ms puis gèle l'écran afin que le joueur puisse regarder les cartes.
+        // Gèle l'écran après 10ms pour éviter que l'écran soit vide lorsqu'il est gelé
         new Timer(10, e -> {
             try {
-                Thread.sleep(5000);
+                Thread.sleep(3000);
             } catch (InterruptedException ex) {
                 throw new RuntimeException(ex);
             }
         }) {{
-            setRepeats(false); // Ne se lance qu'une seule fois
+            // Le timer ne se lance qu'une seule fois
+            setRepeats(false);
             start();
         }};
     }
